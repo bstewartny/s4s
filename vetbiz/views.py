e316a7e32f71bdbe5def308d491132f42f1f9e85
Robert Stewart <bstewart.ny@gmail.com>
2013-08-23 13:49:31 -0400

    Added detail templates

--- vetbiz/views.py
+++ vetbiz/views.py
@@ -5,11 +5,29 @@ from django.template import RequestContext
 import json
 from vetbiz.models import *
 
+def checkins(request):
+    return render_to_response('vetbiz/checkins.html',{'query':request.GET.get('q',''),'user':request.user,'checkins':Checkin.objects.all()})
+
 def offers(request):
     return render_to_response('vetbiz/offers.html',{'query':request.GET.get('q',''),'user':request.user,'offers':Offer.objects.all()})
 
+def offer(request):
+    return render_to_response('vetbiz/offer.html',{'query':request.GET.get('q',''),'user':request.user,'offer':{}})
+
+def profile(request):
+    return render_to_response('vetbiz/profile.html',{'query':request.GET.get('q',''),'user':request.user,'profile':{}})
+
+def places(request):
+    return render_to_response('vetbiz/places.html',{'query':request.GET.get('q',''),'user':request.user,'places':Business.objects.all()})
+
+def place(request):
+    return render_to_response('vetbiz/place.html',{'query':request.GET.get('q',''),'user':request.user,'place':{}})
+
 def jobs(request):
     return render_to_response('vetbiz/jobs.html',{'query':request.GET.get('q',''),'user':request.user,'jobs':[]})
+
+def job(request):
+    return render_to_response('vetbiz/job.html',{'query':request.GET.get('q',''),'user':request.user,'job':{}})
 
 def search(request):
     return render_to_response('vetbiz/search.html',{'query':request.GET.get('q',''),'user':request.user})
