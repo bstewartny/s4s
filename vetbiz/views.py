6218af9c61552dc0bc1fac700a7c3f17421522d3
Robert Stewart <bstewart.ny@gmail.com>
2013-08-26 14:33:43 -0400

    Added context aware search; Links to detail pages; Some details

--- vetbiz/views.py
+++ vetbiz/views.py
@@ -1,36 +1,58 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,get_object_or_404
 from django.template import RequestContext
 import json
 from vetbiz.models import *
 
 def checkins(request):
-    return render_to_response('vetbiz/checkins.html',{'query':request.GET.get('q',''),'user':request.user,'checkins':Checkin.objects.all()})
+    return render_to_response('vetbiz/checkins.html',{'context_type':'map','query':request.GET.get('q',''),'user':request.user,'checkins':Checkin.objects.all()})
 
 def offers(request):
-    return render_to_response('vetbiz/offers.html',{'query':request.GET.get('q',''),'user':request.user,'offers':Offer.objects.all()})
+    query=request.GET.get('q','')
+    offers=None
+    if len(query)>0:
+        offers=Offer.objects.filter(title__icontains=query)
+    else:
+        offers=Offer.objects.all()
+    return render_to_response('vetbiz/offers.html',{'context_type':'offers','query':request.GET.get('q',''),'user':request.user,'offers':offers})
 
-def offer(request):
-    return render_to_response('vetbiz/offer.html',{'query':request.GET.get('q',''),'user':request.user,'offer':{}})
+
+def offer(request,offer_id):
+    offer=get_object_or_404(Offer,pk=offer_id)
+    return render_to_response('vetbiz/offer.html',{'context_type':'offers','query':request.GET.get('q',''),'user':request.user,'offer':offer})
 
 def profile(request):
-    return render_to_response('vetbiz/profile.html',{'query':request.GET.get('q',''),'user':request.user,'profile':{}})
+    return render_to_response('vetbiz/profile.html',{'context_type':'map','query':request.GET.get('q',''),'user':request.user,'profile':{}})
 
 def places(request):
-    return render_to_response('vetbiz/places.html',{'query':request.GET.get('q',''),'user':request.user,'places':Business.objects.all()})
+    query=request.GET.get('q','')
+    places=None
+    if len(query)>0:
+        places=Business.objects.filter(name__icontains=query)
+    else:
+        places=Business.objects.all()
+    return render_to_response('vetbiz/places.html',{'context_type':'places','query':request.GET.get('q',''),'user':request.user,'places':places})
 
-def place(request):
-    return render_to_response('vetbiz/place.html',{'query':request.GET.get('q',''),'user':request.user,'place':{}})
+def place(request,place_id):
+    place=get_object_or_404(Business,pk=place_id)
+    return render_to_response('vetbiz/place.html',{'context_type':'places','query':request.GET.get('q',''),'user':request.user,'place':place})
 
 def jobs(request):
-    return render_to_response('vetbiz/jobs.html',{'query':request.GET.get('q',''),'user':request.user,'jobs':Job.objects.all()})
+    query=request.GET.get('q','')
+    jobs=None
+    if len(query)>0:
+        jobs=Job.objects.filter(title__icontains=query)
+    else:
+        jobs=Job.objects.all()
+    return render_to_response('vetbiz/jobs.html',{'context_type':'jobs','query':request.GET.get('q',''),'user':request.user,'jobs':jobs})
 
-def job(request):
-    return render_to_response('vetbiz/job.html',{'query':request.GET.get('q',''),'user':request.user,'job':{}})
+def job(request,job_id):
+    job=get_object_or_404(Job,pk=job_id)
+    return render_to_response('vetbiz/job.html',{'context_type':'jobs','query':request.GET.get('q',''),'user':request.user,'job':job})
 
 def search(request):
-    return render_to_response('vetbiz/search.html',{'query':request.GET.get('q',''),'user':request.user})
+    return render_to_response('vetbiz/search.html',{'context_type':'map','query':request.GET.get('q',''),'user':request.user})
 
 def signin(request):
     return render_to_response('vetbiz/signin.html',RequestContext(request))
